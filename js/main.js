51bbad1cfee3ecbf3f8b3e9fabff1c805ea90ffc
Lloyd Harrison <lloydharrison@gmail.com>
2020-04-04 08:34:35 +0100

    send details added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const connectButton = document.getElementById('connectButton');
 const disconnectButton = document.getElementById('disconnectButton');
-const sendDataButton = document.getElementById('sendDataButton');
+const sendButton = document.getElementById('sendButton');
 const connected = document.getElementById('connected');
 
 let device, sendCharacteristic, receiveCharacteristic;
-
+let bDevice;
 
 const primaryServiceUuid = '136b6c37-4561-47d9-8719-31c8c06a6930';
 const sendCharUuid = '950c5147-555f-41c0-ab03-6225c489b9db';
@@ -27,135 +27,56 @@ connectButton.onclick = async () => {
       ]
     }
   )
-  .then(device => device.gatt.connect())
+  .then(device => {
+    device.addEventListener('gattserverdisconnected', onDisconnected);
+    return device.gatt.connect();
+  })
   .then(server => server.getPrimaryService(primaryServiceUuid))
   .then(service => service.getCharacteristic(sendCharUuid))
   .then(characteristic => {
     // Writing 1 is the signal to reset energy expended.
-    let encoder = new TextEncoder('utf-8');
-    let sendMsg = encoder.encode("hello");
-    return characteristic.writeValue(sendMsg);
-  })
-  .then(_ => {
-    console.log('data sent.');
+    bDevice = characteristic;
+    console.log('Connected');
     connected.style.display = 'block';
     connectButton.style.display = 'none';
     disconnectButton.style.display = 'initial';
   })
-  .catch(error => { console.log(error); });
-}
-
-/*
-
-.then(
-  server => server.getPrimaryService(primaryServiceUuid)
-).then(
-  //service => service.getCharacteristic('heart_rate_control_point')
-  connected.style.display = 'block';
-  connectButton.style.display = 'none';
-  disconnectButton.style.display = 'initial';
-).then(characteristic => {
-  // Writing 1 is the signal to reset energy expended.
-  var resetEnergyExpended = Uint8Array.of(1);
-  return characteristic.writeValue(resetEnergyExpended);
-})
-*/
-
-/*
-var bluetoothDevice;
-
-connectButton.onclick = async () => {
-  let options = {filters: []};
-
-  options.filters.push({name: "BramwellBrown"});
-
-  bluetoothDevice = null;
-  try {
-    console.log('Requesting Bluetooth Device...');
-    bluetoothDevice = await navigator.bluetooth.requestDevice(options);
-    //bluetoothDevice.addEventListener('gattserverdisconnected', onDisconnected);
-    connect();
-  } catch(error) {
-    console.log('Argh! ' + error);
-  }
-};
-*/
-/*
-async function connect() {
-  console.log('Connecting to Bluetooth Device...');
-  //await bluetoothDevice.gatt.connect();
-
-  const server = await bluetoothDevice.gatt.connect();
-  //const service = await server.getPrimaryService(primaryServiceUuid);
-  //sendCharacteristic = await service.getCharacteristic(sendCharUuid);
-
-  console.log('> Bluetooth Device connected');
-  connected.style.display = 'block';
-  connectButton.style.display = 'none';
-  disconnectButton.style.display = 'initial';
+  .catch(
+    error => {
+      console.log('No clocks found - ERROR' + error);
+      document.getElementById("error-msg").innerHTML = "No Bramwell Brown clocks were found, please put your clock into bluetooth mode and press the connect button again, thank you.";
+    }
+  );
 }
 
 disconnectButton.onclick = async () => {
-  onDisconnectButtonClick();
-}
-
-function onDisconnectButtonClick() {
-  if (!bluetoothDevice) {
+  connected.style.display = 'none';
+  connectButton.style.display = 'display';
+  disconnectButton.style.display = 'none';
+  if (!device) {
     return;
   }
   console.log('Disconnecting from Bluetooth Device...');
-  if (bluetoothDevice.gatt.connected) {
-    bluetoothDevice.gatt.disconnect();
-    connected.style.display = 'none';
-    connectButton.style.display = 'initial';
-    disconnectButton.style.display = 'none';
+  if (device.gatt.connected) {
+    device.gatt.disconnect();
   } else {
     console.log('> Bluetooth Device is already disconnected');
   }
 }
 
 function onDisconnected(event) {
-  // Object event.target is Bluetooth Device getting disconnected.
-  log('> Bluetooth Device disconnected');
-}
-*/
-/*
-function onReconnectButtonClick() {
-  if (!bluetoothDevice) {
-    return;
-  }
-  if (bluetoothDevice.gatt.connected) {
-    console.log('> Bluetooth Device is already connected');
-    return;
-  }
-  try {
-    connect();
-  } catch(error) {
-    console.log('Argh! ' + error);
-  }
+  let device = event.target;
+  document.getElementById("error-msg").innerHTML = 'Device ' + device.name + ' is disconnected.';
+  console.log('Device ' + device.name + ' is disconnected.');
 }
 
-sendDataButton.onclick = async () => {
-  const data = new Uint8Array("Hello");
-  sendCharacteristic.writeValue(data);
-};
+sendButton.onclick = async () => {
+  let ssid = document.getElementById("ssid").value;
+  let pwd = document.getElementById("pwd").value;
 
+  let encoder = new TextEncoder('utf-8');
+  let sendMsg = encoder.encode(ssid + ":" + pwd);
 
-connectButton.onclick = async () => {
-  console.log("connected");
-  device = await navigator.bluetooth
-             .requestDevice({
-                filters: [{
-                  name: "BramwellBrown"
-                }]
-             });
-  const server = await device.gatt.connect();
-  /*const service = await server.getPrimaryService(primaryServiceUuid);
-  receiveCharacteristic = await service.getCharacteristic(receiveCharUuid);
-  sendCharacteristic = await service.getCharacteristic(sendCharUuid);
-
-  device.ongattserverdisconnected = disconnect;
-  connected.style.display = 'block';
-  connectButton.style.display = 'none';
-  disconnectButton.style.display = 'initial';
-};*/
+  console.log(sendMsg);
+  bDevice.writeValue(sendMsg);
+}
