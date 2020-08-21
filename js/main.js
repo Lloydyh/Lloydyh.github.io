6a24d03696d8f4da4cf072823682c518dae6cf73
Lloyd Harrison <lloydharrison@gmail.com>
2020-08-21 17:11:33 +0100

    adde show to show connect button

--- js/main.js
+++ js/main.js
@@ -78,6 +78,7 @@ $(document).ready(function(){
   $("#goto_location_button").click(function(){
     console.log("Enter Location");
     $("#enter_location").hide();
+    $("#connect_button").show();
   });
 
   $("#search_location_button").click(function(){
